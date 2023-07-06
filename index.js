eeaf78462db7e1fac02ee3438857c4496c780f66
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-06 00:38:19 -1000

    Add to index.html and style.css, pseudo index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// define variables w/ doc query selectors
+
+// addEventListener for buttons/etc.
+
+// have a function to make the grid based on user input (also have default size 16x16)
+
+// make a draw function - event listener for hover?
+
+// user input for grid size gets copied for Y (user input = X)
+
+// error message for invalid input (num too large or NaN)
+
+// default 16x16, else userInput
+
+// make grid function
+
+// call the draw function... loop?
+
+// function for color change on hover
+
+// clear grid function

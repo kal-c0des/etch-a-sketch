cdd3a61eb77907d7a8562d6ece22718c066ead1f
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-09 16:20:29 -1000

    Edit style.css and start on index.js functions

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // define variables w/ doc query selectors
+let container = document.querySelector(".container");
+let userInput = document.getElementById("user-input");
+let submit = document.getElementById("submit");
+let copyInput = document.querySelector(".copy-input");
+let prompt = document.querySelector(".prompt");
 
 // addEventListener for buttons/etc.
+submit.addEventListener("click", makeGrid);
 
 // have a function to make the grid based on user input (also have default size 16x16)
 
@@ -13,6 +19,17 @@
 // default 16x16, else userInput
 
 // make grid function
+
+function makeGrid() {
+  let num = userInput.value;
+  if (num <= 0 || num > 100 || isNaN(num)) {
+    prompt.textContent = "Enter a number between 2 and 100!";
+  } else {
+    prompt.textContent = "";
+    copyInput.textContent = "";
+    userInput.value = "";
+  }
+}
 
 // call the draw function... loop?
 

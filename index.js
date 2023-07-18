5f13048a4f607d4a8bd30165d1d5f78fe6c9670f
Kalsie Holt <kalsie@student.dev-island.org>
2023-07-17 21:55:54 -1000

    Remove comments

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 // define variables w/ doc query selectors
-let container = document.querySelector(".container");
-let userInput = document.getElementById("user-input");
-let submit = document.getElementById("submit");
-let copyInput = document.querySelector(".copy-input");
-let prompt = document.querySelector(".prompt");
+const container = document.querySelector(".container");
+const reset = document.getElementById("reset-btn");
 
 // addEventListener for buttons/etc.
-submit.addEventListener("click", makeGrid);
-
-// have a function to make the grid based on user input (also have default size 16x16)
-
-// make a draw function - event listener for hover?
-
-// user input for grid size gets copied for Y (user input = X)
-
-// error message for invalid input (num too large or NaN)
-
-// default 16x16, else userInput
+container.addEventListener("mouseover", (evt) => {
+  // console.log("quah");
+  evt.target.classList.add("hover");
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  evt.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
 
 // make grid function
-
-function makeGrid() {
-  let num = userInput.value;
-  if (num <= 0 || num > 100 || isNaN(num)) {
-    prompt.textContent = "Enter a number between 2 and 100!";
-  } else {
-    prompt.textContent = "";
-    copyInput.textContent = "";
-    userInput.value = "";
+function createGrid(size) {
+  for (let rows = 0; rows < size; rows++) {
+    const rowEm = document.createElement("div");
+    rowEm.classList.add("js-row");
+    container.appendChild(rowEm);
+    for (let cols = 0; cols < size; cols++) {
+      const colsEm = document.createElement("div");
+      colsEm.classList.add("js-col");
+      rowEm.appendChild(colsEm);
+    }
   }
 }
 
-// call the draw function... loop?
+function getPrompt(promptString) {
+  const sizeStr = prompt(promptString);
+  return parseInt(sizeStr);
+}
 
-// function for color change on hover
+const btn = document.getElementById("create");
+btn.addEventListener("click", (e) => {
+  let choice;
+  let isValid;
+  let promptStr = "Enter grid size";
+  do {
+    choice = getPrompt(promptStr);
+    const lessThan100 = choice < 101;
+    const isANum = !isNaN(choice);
+    isValid = lessThan100 && isANum;
+    if (!isValid) {
+      promptStr = "Enter a number less than 100!";
+    }
+  } while (!isValid);
+
+  container.innerHTML = "";
+  createGrid(choice);
+});
+
+const defaultGrid = createGrid(16);
 
 // clear grid function
+reset.addEventListener("click", () => {
+  container.innerHTML = "";
+  createGrid(16);
+});
